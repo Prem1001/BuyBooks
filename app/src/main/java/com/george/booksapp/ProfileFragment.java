9a661ca9381bd2cd7cb5a475f3c6756c4936e089
krupalshah <prem@dataverseinfotech.com>
2022-04-23 20:25:59 +0530

    initial commit

---
+++ app/src/main/java/com/george/booksapp/ProfileFragment.java
@@ -0,0 +1,22 @@
+package com.george.booksapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * this class represents the ui for profile screen - will be added in the future
+ */
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
